31a90274d8f52c13834ae5aced757da3f30ff5dc
Maxwell J Branvall <maxbranvall@gmail.com>
2018-10-12 01:05:47 -0500

    Scraper.py properly sets up csvTable.csv with basic game data

--- DWG_BOT.py
+++ DWG_BOT.py
@@ -1,16 +1,15 @@
 import requests
 from lxml import html
 import praw
-
-majNelsonURL = 'https://majornelson.com/2018/10/08/this-weeks-deals-with-gold-and-spotlight-sale-135/'
-
-header = { 
-    'USER-AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-}
+import scraper
 
 redditInstance = praw.Reddit('dwgBot')
 
 testSub = redditInstance.subreddit('test')
+
+# scraper.main()
+
+print(redditInstance.user.me())
 
 # testSub.submit('FormatTest2', selftext= '**This is bold**, *These are italics*,\
 # `this is inline code`,\
@@ -24,13 +23,3 @@ testSub = redditInstance.subreddit('test')
 # ---|---\n\
 # row | row')
 #         break
-
-# majorNelsonSite = requests.get(majNelsonURL, headers= header)
-# majorNelsonSiteContent = html.fromstring(majorNelsonSite.content)
-
-# title = majorNelsonSiteContent.xpath('//*[@id="post-36186"]/header/div[1]/h1/text()')
-
-# print(title[0])
-
-print(redditInstance.user.me())
-# print(redditInstance.read_only)
---
+++ scraper.py
@@ -0,0 +1,119 @@
+import requests, csv
+from bs4 import BeautifulSoup
+from lxml import html
+
+headerList = []
+gameDataList = []
+csvTableFile = 'csvTable.csv'
+
+header = { 
+    'USER-AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+}
+
+testHeader = {
+    'USER-AGENT' : 'TestBot'
+}
+
+majNelsonURL = 'https://majornelson.com/2018/10/08/this-weeks-deals-with-gold-and-spotlight-sale-135/'
+trueAchievementsURL = 'https://www.trueachievements.com/game/'
+
+class Utility:
+
+    def clearFile(self, filePath):
+        with open(filePath, 'w') as foo:
+            pass
+
+class MajorNelsonScrape(Utility):
+
+    def __init__(self):
+
+        self.clearFile(csvTableFile)
+
+        x = open('rawhtml.html', 'r')
+        self.nelsonSoup = BeautifulSoup(x, 'html.parser')
+        tableTitles = self.nelsonSoup.find_all('h4')
+
+        self.writeToCsv = csv.writer(open(csvTableFile, 'a'))
+        self.xOneTableTitle = tableTitles[0].text
+        self.x360TableTitle = tableTitles[1].text
+
+        self.getTableHeaders()
+
+    def getTableHeaders(self):
+        
+        for data in self.nelsonSoup.find_all('tr'):
+
+            headerList.clear()
+            tableHeader = data.find_all('th')
+
+            if tableHeader == []:
+                continue
+
+            else:
+
+                i = 0
+                for item in tableHeader:
+
+                    if i == 3:
+                        break
+                    else:
+                        headerList.append(item.text)
+                    i += 1
+            break
+
+        self.getTableContents()
+
+    def getTableContents(self):
+
+        num = 0
+
+        self.writeToCsv.writerow(['Xbox One Table'])
+        self.writeToCsv.writerow(headerList)
+        
+        for data in self.nelsonSoup.find_all('tr')[1:]:
+
+            try:
+                if num == 100:
+                    break
+
+            except IndexError:
+                break
+
+            gameDataList.clear()
+
+            gameData = data.find_all('td')
+
+            if gameData == []:
+                print('End of Xbox One Deals')
+                self.writeToCsv.writerow([])
+                self.writeToCsv.writerow(['Xbox 360 Table'])
+                self.writeToCsv.writerow(headerList)
+
+            else:
+
+                i = 0
+                for item in gameData:
+
+                    if i == 3:
+                        break
+                    else:
+                        gameDataList.append(item.text)
+                    i += 1
+            
+            if gameDataList == []:
+                continue
+
+            else:
+                self.writeToCsv.writerow(gameDataList)
+
+            num += 1
+
+class TrueAchievementsScrape:
+    pass
+
+class HowLongToBeatScrape:
+    pass
+
+if __name__ == '__main__':
+    MajorNelsonScrape()
+    print('Success!')
